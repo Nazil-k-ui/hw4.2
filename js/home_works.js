9dec78dab397f5c1787b77f244f97d93fd02bd30
Nazik-k-ui <rayanajung123@gmail.com>
2025-07-06 22:11:01 +0300

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,45 @@
-// MOVE BLOCK HW 1.2
+// phone block
 
-const parentBlock = document.querySelector('.parent_block');
-const childBlock = document.querySelector('.child_block');
+const gmailInput = document.querySelector("#gmail_input")
+const gmailButton = document.querySelector("#gmail_button")
+const gmailResult = document.querySelector("#gmail_result")
 
-let positionX = 0
-let positionY = 0
+const regExp = /^[a-zA-Z0-9._%+-]{3,}@gmail\.com$/;
 
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
-
-const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
+gmailButton.onclick =() => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML ="OK"
+        gmailResult.style.color='green'
+    }
+        else {
+            gmailResult.innerHTML ="ERROR"
+            gmailResult.style.color="red"
+        }
 }
 
-moveBlock();
+
+//animation block
+let number = 0;
+const block = document.querySelector('.child_block');
+
+const count = () => {
+    number++;
+    block.style.left = number + "px";
+
+    if (number < 447) {
+        requestAnimationFrame(count); 
+    }
+};
+
+ window.onload = () => {
+      count();
+ }
+
+// let number=0
+// const count = () => {
+//     number++
+//     console.log(number)
+//     if (number < 450) {
+//         count
+//     }
+// }
